fb08cc8f4204f5f4124d1cbc70b7d72d218f8f51
Vadim Safroshkin <vadimpuskin818@gmail.com>
2026-05-04 23:32:02 +0700

    выполненное задание lesson02

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Типы данных, операторы, методы и свойства";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 2438;
+let rollback = 2;
+let fullPrice = 48000;
+let adaptive = false;
 
-alert("Любой текст!)");
-console.log("Любой текст в консоли!)");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов", screenPrice, "рублей");
+console.log("Стоимость разработки сайта", fullPrice, "рублей");
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
